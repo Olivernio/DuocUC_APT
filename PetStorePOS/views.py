e2b7eb2ebb51a68ff66f5e2ed06420e50188c9ac
Martín Mondaca <mar.mondacam@duocuc.cl>
2025-09-12 18:21:00 -0300

    Funciones realizadas : Template y static configurados estoy viendo el entorno de trabajo

--- PetStorePOS/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-
-# Create your views here.
-def Hello (request):
-    return HttpResponse("Hello World")
--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PetstoreposConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PetStorePOS'
+    name = 'core'
